19ed7b37d42f1379df3d603ea1fef89d7c8c27e9
victor.contogouris <vcontogouris@gmail.com>
2022-01-18 20:34:49 +0100

    Try to shorten the code at the end bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -256,8 +256,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
-            break
-
+            break 
 
 if __name__ == "__main__":
 	main()
